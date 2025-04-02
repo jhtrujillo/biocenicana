14c34794586c9dbb561fe8a37e2186241dbe744f
jhtrujillo <jhon.murdock@gmail.com>
2025-04-02 18:14:43 -0500

    New commits

--- lib/src/main/java/biocenicana/Test.java
+++
@@ -1,5 +0,0 @@
-package biocenicana;
-
-public class Test {
-
-}
---
+++ lib/src/main/java/vcf/VCF.java
@@ -0,0 +1,15 @@
+package vcf;
+
+import htsjdk.variant.variantcontext.VariantContext;
+import htsjdk.variant.vcf.VCFFileReader;
+import htsjdk.variant.vcf.VCFHeader;
+
+
+public class VCF {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
