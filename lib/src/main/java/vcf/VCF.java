95d72d126c0d2343ff47337cac279db5cfcfa296
jhtrujillo <jhon.murdock@gmail.com>
2025-04-02 20:49:04 -0500

    New commits

--- lib/src/main/java/vcf/VCF.java
+++ lib/src/main/java/vcf/VCF.java
@@ -8,74 +8,96 @@ import java.io.File;
 import java.util.Iterator;
 import java.util.Map;
 
+public class VCF {
 
+    private VCFHeader header;
+    private Iterator<VariantContext> variantsIterator;
 
-	public class VCF {
+    /**
+     * Constructor that reads the VCF file.
+     * @param vcfFilePath Path to the VCF file.
+     */
+    public VCF(String vcfFilePath) {
+        File vcfFile = new File(vcfFilePath);
+        VCFFileReader reader = new VCFFileReader(vcfFile, false);
 
-	    private VCFHeader header;
-	    private Iterator<VariantContext> variantsIterator;
+        this.header = reader.getFileHeader();
+        this.variantsIterator = reader.iterator();
+    }
 
-	    // Constructor que lee el archivo VCF
-	    public VCF(String vcfFilePath) {
-	        File vcfFile = new File(vcfFilePath);
-	        VCFFileReader reader = new VCFFileReader(vcfFile, false);
+    /**
+     * Method to get the header.
+     * @return The VCF file header.
+     */
+    public VCFHeader getHeader() {
+        return header;
+    }
 
-	        this.header = reader.getFileHeader();
-	        this.variantsIterator = reader.iterator();
-	    }
+    /**
+     * Method to set the header.
+     * @param header The VCF file header.
+     */
+    public void setHeader(VCFHeader header) {
+        this.header = header;
+    }
 
-	    // Método para obtener el encabezado
-	    public VCFHeader getHeader() {
-	        return header;
-	    }
+    /**
+     * Method to get the next VariantContext (a variant).
+     * @return The next VariantContext, or null if there are no more variants.
+     */
+    public VariantContext getNextVariant() {
+        if (variantsIterator.hasNext()) {
+            return variantsIterator.next();
+        }
+        return null;  // If there are no more variants
+    }
 
-	    // Método para obtener el siguiente VariantContext (una variante)
-	    public VariantContext getNextVariant() {
-	        if (variantsIterator.hasNext()) {
-	            return variantsIterator.next();
-	        }
-	        return null;  // Si no hay más variantes
-	    }
+    /**
+     * Method to iterate over all variants.
+     */
+    public void iterateVariants() {
+        while (variantsIterator.hasNext()) {
+            VariantContext variant = variantsIterator.next();
+            printVariantDetails(variant);  // You can change this method to process the variant as you prefer
+        }
+    }
 
-	    // Método para iterar sobre todas las variantes
-	    public void iterateVariants() {
-	        while (variantsIterator.hasNext()) {
-	            VariantContext variant = variantsIterator.next();
-	            printVariantDetails(variant);  // Puedes cambiar este método para procesar la variante como prefieras
-	        }
-	    }
+    /**
+     * Method to print the details of a variant.
+     * @param variant The VariantContext to print.
+     */
+    @SuppressWarnings("deprecation")
+    private void printVariantDetails(VariantContext variant) {
+        System.out.print(variant.getChr() + "\t");
+        System.out.print(variant.getStart() + "\t");
+        System.out.print(variant.getReference() + "\t");
+        System.out.print(variant.getAlternateAlleles() + "\t");
 
-	    // Método para imprimir los detalles de una variante
-	    @SuppressWarnings("deprecation")
-		private void printVariantDetails(VariantContext variant) {
-	        System.out.print(variant.getChr()+"\t");
-	        System.out.print(variant.getStart()+"\t");
-	        System.out.print(variant.getReference()+"\t");
-	        System.out.print(variant.getAlternateAlleles()+"\t");
-	        //System.out.print(variant.getGenotypes()+"\t");
-	        
-	        for (Genotype genotype : variant.getGenotypes()) {
-	        	Map<String, Object> attributes = genotype.getExtendedAttributes();
-	        	String BSDP = (String) attributes.get("BSDP");  // Base Depth (BSDP) es un campo común en FORMAT
-	            String ACN = (String) attributes.get("ACN"); // Allele Count (ACN)
-	            System.out.print("["+genotype.getGenotypeString()+"|"+BSDP+"|"+ACN+"]\t");
-	        }
-	             
-	        System.out.println("");
-	    }
+        for (Genotype genotype : variant.getGenotypes()) {
+            Map<String, Object> attributes = genotype.getExtendedAttributes();
+            String BSDP = (String) attributes.get("BSDP");  // Base Depth (BSDP) is a common field in FORMAT
+            String ACN = (String) attributes.get("ACN"); // Allele Count (ACN)
+            System.out.print("[" + genotype.getGenotypeString() + "|" + BSDP + "|" + ACN + "]\t");
+        }
 
-	    // Main para probar la clase
-	    public static void main(String[] args) {
-	        String vcfFilePath = "/Users/estuvar4/Downloads/tmp2.vcf";  // Cambia la ruta al archivo VCF que deseas leer
-	        VCF vcfReader = new VCF(vcfFilePath);
+        System.out.println("");
+    }
 
-	        // Obtener e imprimir el encabezado
-	        VCFHeader header = vcfReader.getHeader();
-	        System.out.println("Header del VCF:");
-	        System.out.println(header);
+    /**
+     * Main method to test the class.
+     * @param args Command line arguments.
+     */
+    public static void main(String[] args) {
+        String vcfFilePath = "/Users/estuvar4/Downloads/tmp2.vcf";  // Change the path to the VCF file you want to read
+        VCF vcfReader = new VCF(vcfFilePath);
 
-	        // Iterar sobre las variantes e imprimir detalles
-	        System.out.println("\nVariantes:");
-	        vcfReader.iterateVariants();
-	    }
-	}
+        // Get and print the header
+        VCFHeader header = vcfReader.getHeader();
+        System.out.println("VCF Header:");
+        System.out.println(header);
+
+        // Iterate over the variants and print details
+        System.out.println("\nVariants:");
+        vcfReader.iterateVariants();
+    }
+}
