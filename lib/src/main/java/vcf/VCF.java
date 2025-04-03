ec428813bafe19b94a19109e9fdf133fd4682dad
jhtrujillo <jhon.murdock@gmail.com>
2025-04-02 19:39:10 -0500

    New commits

--- lib/src/main/java/vcf/VCF.java
+++ lib/src/main/java/vcf/VCF.java
@@ -1,10 +1,12 @@
 package vcf;
 
+import htsjdk.variant.variantcontext.Genotype;
 import htsjdk.variant.variantcontext.VariantContext;
 import htsjdk.variant.vcf.VCFFileReader;
 import htsjdk.variant.vcf.VCFHeader;
 import java.io.File;
 import java.util.Iterator;
+import java.util.Map;
 
 
 
@@ -17,11 +19,8 @@ import java.util.Iterator;
 	    public VCF(String vcfFilePath) {
 	        File vcfFile = new File(vcfFilePath);
 	        VCFFileReader reader = new VCFFileReader(vcfFile, false);
-	        
-	        // Obtén el encabezado del archivo VCF
+
 	        this.header = reader.getFileHeader();
-	        
-	        // Obtén el iterador de variantes
 	        this.variantsIterator = reader.iterator();
 	    }
 
@@ -47,18 +46,27 @@ import java.util.Iterator;
 	    }
 
 	    // Método para imprimir los detalles de una variante
-	    private void printVariantDetails(VariantContext variant) {
-	        System.out.println("ID: " + variant.getID());
-	        System.out.println("Genotipos: " + variant.getGenotypes());
-	        System.out.println("Posición: " + variant.getStart());
-	        System.out.println("Referencia: " + variant.getReference());
-	        System.out.println("Alelo alternativo(s): " + variant.getAlternateAlleles());
-	        System.out.println("-------------------");
+	    @SuppressWarnings("deprecation")
+		private void printVariantDetails(VariantContext variant) {
+	        System.out.print(variant.getChr()+"\t");
+	        System.out.print(variant.getStart()+"\t");
+	        System.out.print(variant.getReference()+"\t");
+	        System.out.print(variant.getAlternateAlleles()+"\t");
+	        //System.out.print(variant.getGenotypes()+"\t");
+	        
+	        for (Genotype genotype : variant.getGenotypes()) {
+	        	Map<String, Object> attributes = genotype.getExtendedAttributes();
+	        	String BSDP = (String) attributes.get("BSDP");  // Base Depth (BSDP) es un campo común en FORMAT
+	            String ACN = (String) attributes.get("ACN"); // Allele Count (ACN)
+	            System.out.print("["+genotype.getGenotypeString()+"|"+BSDP+"|"+ACN+"]\t");
+	        }
+	             
+	        System.out.println("");
 	    }
 
 	    // Main para probar la clase
 	    public static void main(String[] args) {
-	        String vcfFilePath = "/Users/estuvar4/Downloads/tmp.vcf";  // Cambia la ruta al archivo VCF que deseas leer
+	        String vcfFilePath = "/Users/estuvar4/Downloads/tmp2.vcf";  // Cambia la ruta al archivo VCF que deseas leer
 	        VCF vcfReader = new VCF(vcfFilePath);
 
 	        // Obtener e imprimir el encabezado
